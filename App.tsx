406b5314fa5415f6bf0b0c73fc5fad15e94f2f53
ushna-parvaiz <ushnapervaiz10@gmail.com>
2025-10-06 21:04:20 +0500

    updated!!!

--- App.tsx
+++ App.tsx
@@ -3,8 +3,8 @@ import { StatusBar, StyleSheet, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 // import HomeListening from "./src/screens/HomeListening";
 // import HomeScreen from "./src/screens/HomeScreen";
-import Signup from "./src/screens/Signup";
-// import TranscriptScreen from "./src/screens/TranscriptScreen";
+// import Signup from "./src/screens/Signup";
+import TranscriptScreen from "./src/screens/TranscriptScreen";
 // import LoginScreen from "./src/screens/LoginScreen";
 
 const App: React.FC = () => {
@@ -15,8 +15,8 @@ const App: React.FC = () => {
         {/* <HomeListening /> */}
         {/* <HomeScreen /> */}
         {/* <LoginScreen /> */}
-        <Signup />
-        {/* <TranscriptScreen /> */}
+        {/* <Signup /> */}
+        <TranscriptScreen />
       </View>
     </SafeAreaProvider>
   );
